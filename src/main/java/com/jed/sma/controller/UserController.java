40ca2773e580895e471e989d52cbe5a585859af7
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-05 14:30:26 -0700

    tested openapi definitions on localhost

--- src/main/java/com/jed/sma/controller/UserController.java
+++ src/main/java/com/jed/sma/controller/UserController.java
@@ -8,32 +8,36 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/v1/users")
-public class UserController {
-
-  private List<User> users = new ArrayList<>();
-
-//  @GetMapping(path = "/v1/helloworld")
-//  public String helloWorld() {
-//    return "Hello World";
-//  }
-
-  @GetMapping
+public class UserController  implements UserOperation{
+  @Override
   public List<User> getUsers() {
-    //    User user1 = new User();
-    //    user1.setUserId(1);
-    //    user1.setUserName("fHampton");
-    //    users.add(user1);
-    //
-    //    User user2 = new User();
-    //    user2.setUserId(1);
-    //    user2.setUserName("dParton");
-    //    users.add(user2);
-    return users;
+    return null;
   }
 
-  @PostMapping
-  public User addUser(@RequestBody User user) {
-    users.add(user);
-    return user;
-  }
+//  private List<User> users = new ArrayList<>();
+//
+////  @GetMapping(path = "/v1/helloworld")
+////  public String helloWorld() {
+////    return "Hello World";
+////  }
+//
+//  @GetMapping
+//  public List<User> getUsers() {
+//    //    User user1 = new User();
+//    //    user1.setUserId(1);
+//    //    user1.setUserName("fHampton");
+//    //    users.add(user1);
+//    //
+//    //    User user2 = new User();
+//    //    user2.setUserId(1);
+//    //    user2.setUserName("dParton");
+//    //    users.add(user2);
+//    return users;
+//  }
+//
+//  @PostMapping
+//  public User addUser(@RequestBody User user) {
+//    users.add(user);
+//    return user;
+//  }
 }
--- src/main/java/com/jed/sma/controller/UserOperation.java
+++ src/main/java/com/jed/sma/controller/UserOperation.java
@@ -1,11 +1,54 @@
 package com.jed.sma.controller;
 
 import com.jed.sma.entity.User;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
+@RequestMapping("/v1/users")
+@OpenAPIDefinition(
+    info = @Info(title = "User API"),
+    servers = {@Server(url = "http://localhost:8080", description = "Local Server")})
 public interface UserOperation {
 
-    List<User> getUsers(){return null; }
+  @Operation(
+      summary = "Returns a list of users",
+      description = "Returns a list of users",
+      responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "A list of users is returned",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = User.class))),
+              @ApiResponse(
+                      responseCode = "500",
+                      description = "Internal Server Error Occurred",
+                      content =
+                      @Content(
+                              mediaType = MediaType.APPLICATION_JSON_VALUE)),
+              @ApiResponse(
+                      responseCode = "404",
+                      description = "Page Not Found - User Error",
+                      content =
+                      @Content(
+                              mediaType = MediaType.APPLICATION_JSON_VALUE))
 
+      })
+
+  @GetMapping
+  @ResponseStatus(code = HttpStatus.OK)
+  List<User> getUsers();
 }
