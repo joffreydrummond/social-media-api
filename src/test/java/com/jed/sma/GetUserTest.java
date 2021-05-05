4c18675e89b0aa5caf07ce3af35fdd087c72a397
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-05 14:04:51 -0700

    created response entity object of baseuri and user class

--- src/test/java/com/jed/sma/GetUserTest.java
+++ src/test/java/com/jed/sma/GetUserTest.java
@@ -1,8 +1,10 @@
 package com.jed.sma;
 
+import com.jed.sma.entity.User;
 import com.jed.sma.support.GetUserTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GetUserTest extends GetUserTestSupport {
@@ -16,6 +18,7 @@ class GetUserTest extends GetUserTestSupport {
 	@Test
 	void getUsers_users_returnsListOfUsers(){
 
+	ResponseEntity<User> responseEntity = getRestTemplate().getForEntity(getBaseUri(), User.class);
 
 	}
 
