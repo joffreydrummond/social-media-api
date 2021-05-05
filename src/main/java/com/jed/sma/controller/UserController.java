8878d1f5401cda9ba9fa1525a0cf6d2581ade100
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-05 14:37:44 -0700

    updated application.properties logging level no more long words only important ones

--- src/main/java/com/jed/sma/controller/UserController.java
+++ src/main/java/com/jed/sma/controller/UserController.java
@@ -1,16 +1,19 @@
 package com.jed.sma.controller;
 
 import com.jed.sma.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/v1/users")
 public class UserController  implements UserOperation{
   @Override
   public List<User> getUsers() {
+    log.debug("Inside getUsers() in UserController");
     return null;
   }
 
